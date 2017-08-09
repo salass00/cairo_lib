87b15e3c4e3cf17142b4bf8b145a5e5718903dd4
Fredrik Wikstrom <fredrik@a500.org>
2017-08-09 23:12:57 +0300

    Added h/w acceleration for alpha blended rectangles.

--- cairo-1.14.10/src/amigaos/cairo-amigaos-compositor.c
+++ cairo-1.14.10/src/amigaos/cairo-amigaos-compositor.c
@@ -31,9 +31,10 @@
 #include "cairo-clip-inline.h"
 #include "cairo-compositor-private.h"
 
+#include <graphics/composite.h>
 #include <proto/graphics.h>
 
-#define ARGB(a, r, g, b) (((a) << 24) | ((r) << 16) | ((g) << 8) | (b))
+#define ARGB(a, r, g, b) (((uint32_t)(a) << 24) | ((uint32_t)(r) << 16) | ((uint32_t)(g) << 8) | (uint32_t)(b))
 
 static cairo_bool_t
 fill_box (cairo_box_t *box, void *user_data)
@@ -74,6 +75,117 @@ fill_boxes (cairo_amigaos_surface_t *dst,
 	return status;
 }
 
+typedef struct {
+	float x, y, s, t, w;
+} amigaos_vertex_t;
+
+struct alpha_box {
+	struct BitMap *src, *dst;
+	uint8_t        alpha;
+};
+
+static cairo_bool_t alpha_box (cairo_box_t *box, void *user_data)
+{
+	struct alpha_box *ab = user_data;
+	amigaos_vertex_t  vertices[6];
+	int               x1, y1, x2, y2;
+	uint32            error;
+
+	x1 = _cairo_fixed_integer_part(box->p1.x);
+	y1 = _cairo_fixed_integer_part(box->p1.y);
+	x2 = _cairo_fixed_integer_part(box->p2.x);
+	y2 = _cairo_fixed_integer_part(box->p2.y);
+
+	vertices[0].x = x1;
+	vertices[0].y = y1;
+	vertices[0].s = 0;
+	vertices[0].t = 0;
+	vertices[0].w = 1;
+
+	vertices[1].x = x2;
+	vertices[1].y = y1;
+	vertices[1].s = 1;
+	vertices[1].t = 0;
+	vertices[1].w = 1;
+
+	vertices[2].x = x1;
+	vertices[2].y = y2;
+	vertices[2].s = 0;
+	vertices[2].t = 1;
+	vertices[2].w = 1;
+
+	vertices[3].x = x1;
+	vertices[3].y = y2;
+	vertices[3].s = 0;
+	vertices[3].t = 1;
+	vertices[3].w = 1;
+
+	vertices[4].x = x2;
+	vertices[4].y = y1;
+	vertices[4].s = 1;
+	vertices[4].t = 0;
+	vertices[4].w = 1;
+
+	vertices[5].x = x2;
+	vertices[5].y = y2;
+	vertices[5].s = 1;
+	vertices[5].t = 1;
+	vertices[5].w = 1;
+
+	error = IGraphics->CompositeTags(COMPOSITE_Src_Over_Dest, ab->src, ab->dst,
+		COMPTAG_Flags,        COMPFLAG_HardwareOnly,
+		COMPTAG_VertexArray,  vertices,
+		COMPTAG_VertexFormat, COMPVF_STW0_Present,
+		COMPTAG_NumTriangles, 2,
+		COMPTAG_SrcAlpha,     (ab->alpha * 0x10000UL) / 255,
+		TAG_END);
+	if (unlikely (error != COMPERR_Success))
+		return FALSE;
+
+	return TRUE;
+}
+
+static cairo_int_status_t
+alpha_blend_boxes (cairo_amigaos_surface_t *dst,
+                   const cairo_pattern_t   *src,
+                   cairo_boxes_t           *boxes,
+                   uint8_t                  alpha)
+{
+	const cairo_color_t *color  = &((cairo_solid_pattern_t *)src)->color;
+	cairo_int_status_t   status = CAIRO_INT_STATUS_UNSUPPORTED;
+	struct BitMap       *temp_bm;
+	uint32_t            *bm_addr;
+	APTR                 bm_lock;
+	struct alpha_box     ab;
+
+	temp_bm = IGraphics->AllocBitMapTags(1, 1, 24,
+		BMATags_Friend,      dst->bitmap,
+		BMATags_PixelFormat, PIXF_A8R8G8B8,
+		TAG_END);
+	if (temp_bm == NULL)
+		return CAIRO_INT_STATUS_UNSUPPORTED;
+
+	bm_lock = IGraphics->LockBitMapTags(temp_bm,
+		LBM_BaseAddress, &bm_addr,
+		TAG_END);
+
+	/* We don't use the short values here because they have the alpha premultiplied. */
+	*bm_addr = ARGB(color->alpha * 255, color->red * 255, color->green * 255, color->blue * 255);
+
+	IGraphics->UnlockBitMap(bm_lock);
+
+	ab.src   = temp_bm;
+	ab.dst   = dst->bitmap;
+	ab.alpha = alpha;
+
+	if (likely (_cairo_boxes_for_each_box(boxes, alpha_box, &ab)))
+		status = CAIRO_STATUS_SUCCESS;
+
+	IGraphics->FreeBitMap(temp_bm);
+
+	return status;
+}
+
 static cairo_int_status_t
 draw_boxes (cairo_composite_rectangles_t *extents,
             cairo_boxes_t                *boxes)
@@ -99,24 +211,15 @@ draw_boxes (cairo_composite_rectangles_t *extents,
 	    (op == CAIRO_OPERATOR_OVER || op == CAIRO_OPERATOR_ADD))
 		op = CAIRO_OPERATOR_SOURCE;
 
-	if (op == CAIRO_OPERATOR_SOURCE) {
-		cairo_int_status_t status = CAIRO_INT_STATUS_UNSUPPORTED;
+	if (src->type != CAIRO_PATTERN_TYPE_SOLID)
+		return CAIRO_INT_STATUS_UNSUPPORTED;
 
-		if (src->type == CAIRO_PATTERN_TYPE_SOLID)
-			status = fill_boxes(dst, src, boxes);
+	if (op == CAIRO_OPERATOR_SOURCE)
+		return fill_boxes(dst, src, boxes);
 
-		return status;
-	}
+	if (op == CAIRO_OPERATOR_OVER)
+		return alpha_blend_boxes(dst, src, boxes, 255);
 
-	return CAIRO_INT_STATUS_UNSUPPORTED;
-}
-
-static cairo_int_status_t
-alpha_blend_boxes (cairo_amigaos_surface_t *dst,
-                   const cairo_pattern_t   *src,
-                   cairo_boxes_t           *boxes,
-                   uint8_t                  alpha)
-{
 	return CAIRO_INT_STATUS_UNSUPPORTED;
 }
 
